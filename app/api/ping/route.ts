631b4a804a5096dd7c45a35e6705703718309c67
walker <dev.walkeralfaro@gmail.com>
2026-02-11 19:06:31 -0500

    add post

--- app/api/ping/route.ts
+++ app/api/ping/route.ts
@@ -7,3 +7,15 @@ export async function GET() {
     timestamp: Date.now(),
   });
 }
+
+
+export async function POST(req: Request) {
+  const data = await req.json()
+
+  console.log("Datos recibidos:", data)
+
+  // Aquí podrías guardarlo en DB
+  // await prisma.sensor.create({ data })
+
+  return NextResponse.json({ success: true })
+}
